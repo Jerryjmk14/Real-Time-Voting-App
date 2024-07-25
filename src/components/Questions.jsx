4f692d98460e715e00a4ecd9e6c7c2ae82de1a1e
Jeremiah Egwa <jerriee232@gmail.com>
2024-07-25 13:38:18 +0100

    implemented the functionality for the form  handleSubmit

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Vote from "./Vote";
+import { COLLECTION_ID, databases, DB_ID } from "../utils/appewrite";
 
 export default function Questions({ data }) {
+  const totalVotes = data.votes_1 + data.votes_2 + data.votes_3;
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const selectedVote = formData.get("vote");
+
+    if (selectedVote === data.answer_1) {
+      databases.updateDocument(DB_ID, COLLECTION_ID, data.$id, {
+        votes_1: data.votes_1 + 1,
+      });
+    } else if (selectedVote === data.answer_2) {
+      databases.updateDocument(DB_ID, COLLECTION_ID, data.$id, {
+        votes_2: data.votes_2 + 1,
+      });
+    } else if (selectedVote === data.answer_3) {
+      databases.updateDocument(DB_ID, COLLECTION_ID, data.$id, {
+        votes_3: data.votes_3 + 1,
+      });
+    }
   };
+
   return (
     <>
       <h2 className="text-3xl text-center font-bold">{data.text}</h2>
@@ -12,9 +33,21 @@ export default function Questions({ data }) {
       <form
         onSubmit={handleSubmit}
         className="flex flex-col gap-4 votes-container">
-        <Vote text={data.answer_1} percentage={"33%"} votes={data.votes_1} />
-        <Vote text={data.answer_2} percentage={"33%"} votes={data.votes_2} />
-        <Vote text={data.answer_3} percentage={"33%"} votes={data.votes_3} />
+        <Vote
+          text={data.answer_1}
+          percentage={Math.floor((data.votes_1 / totalVotes) * 100)}
+          votes={data.votes_1}
+        />
+        <Vote
+          text={data.answer_2}
+          percentage={Math.floor((data.votes_2 / totalVotes) * 100)}
+          votes={data.votes_2}
+        />
+        <Vote
+          text={data.answer_3}
+          percentage={Math.floor((data.votes_3 / totalVotes) * 100)}
+          votes={data.votes_3}
+        />
 
         <button
           type="submit"
